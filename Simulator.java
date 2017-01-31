9a414d2b63428f830f905ce4b9d996112e32b2d4
agnes-kabaly <alicet.woods@gmail.com>
2017-01-31 15:02:02 +0100

    Simulator started

---
+++ Simulator.java
@@ -0,0 +1,19 @@
+public class Simulator
+{
+    public Simulation simulator;
+    public Logger logger;
+
+
+    public Simulator(Simulation simulator, Logger logger)
+    {
+        this.simulator = simulator;
+	this.logger = logger;
+    }
+
+    public Result run()
+    {
+
+    }
+
+
+}
