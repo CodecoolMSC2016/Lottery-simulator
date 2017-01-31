e0bba0d7faba706b7e02c4b8c57eac0992f8a248
Nikolett Uracs <loveninagold@gmail.com>
2017-01-31 20:53:47 +0100

    random numbers generated ready

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
-public class Main extends Logger {
+public class Main {
 
 	public static void main (String [] args) {
 		int userChoice;
 		
-		userChoice = menu();
+		userChoice = Logger.menu();
+		switch(userChoice) {
+			case 1:
+				Simulation.generateData(5, 90);
+				break;
+			case 2:
+				Simulation.generateData(6, 45);
+				break;
+		}
 	
 	}
 }
--- Simulation.java
+++ Simulation.java
@@ -1,26 +1,30 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+
 public class Simulation 
 {
+ 	public static List<Integer> winningNumbers = new ArrayList<>();
 
-	public Simulation (int year, int month, String date, int number1, int number2, int number3, int number4, int number5){
-		this.year = year;
-		this.month = month;
-		this.date = date;
-		this.number1 = number1;
-		this.number2 = number2;
-		this.number3 = number3;
-		this.number4 = number4;
-		this.number5 = number5;
+	public static void generateData(int numberOfBalls, int endNumber) {
+
+		for (int i = 0; i < numberOfBalls; i++)        {
+            		int number = (int) (Math.ceil(Math.random() * endNumber));
+            
+            		while (true)
+            		{
+                		if (winningNumbers.contains(number))
+                		{
+                    			number = (int) (Math.ceil(Math.random() * endNumber));
+                		}
+                
+                		else
+                		{
+                    			winningNumbers.add(number);
+                    			break;
+                		}
+            		}
+		} System.out.println(winningNumbers);			
 	}
-	public static void generateData() {
-
-	String csvFilename = "otos.csv";
-	CSVReader csvReader = new CSVReader(new FileReader(csvFilename));
-	String[] row = null;
-	while((row = csvReader.readNext()) != null) {
-		Simulation thisround = String[] (year, month, date, number1, number2, number3, number4, number5);
-
-	csvReader.close();
-	System.out.println(row);
-			
-	}
+	
 }
