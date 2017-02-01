8de78136140e444b9ce2ff15ecce79a7f3ad2dc4
Nikolett Uracs <loveninagold@gmail.com>
2017-02-01 22:08:32 +0100

    edited result

--- Result.java
+++ Result.java
@@ -1,14 +1,35 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Result {
-	ArrayList<Integer> resultSimulation = new ArrayList<>();
 
-	public ArrayList<Integer> getResult() {
-	    return resultSimulation;
+	public List<Integer> topNumbers = new ArrayList<>();
+
+	int number1;
+	int number2;
+	int number3;
+	int number4;
+	int number5;
+	int number6;
+
+	public Result() {
+	
+		this.number1 = number1;
+		this.number2 = number2;
+		this.number3 = number3;
+		this.number4 = number4;
+		this.number5 = number5;
+		this.number6 = number6;
 	}
 
-	public void setResult() {
-	    this.
+	public static void getTopNumbers() {
+
+			
+
+	}
+	
 
 
 
