5b0c1e6298c9169d8d645312496f9721f63cdf95
agnes-kabaly <alicet.woods@gmail.com>
2017-02-01 13:45:55 +0100

    result skelleton started

---
+++ Result.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Result {
+	ArrayList<Integer> resultSimulation = new ArrayList<>();
+
+	public ArrayList<Integer> getResult() {
+	    return resultSimulation;
+	}
+
+	public void setResult() {
+	    this.
+
+
+
+
+}
--- Simulator.java
+++ Simulator.java
@@ -4,15 +4,15 @@ public class Simulator
     public Logger logger;
 
 
-    public Simulator(Simulation simulator, Logger logger)
+    public Simulator(Simulation simulation, Logger logger)
     {
-        this.simulator = simulator;
+        this.simulation = simulation;
 	this.logger = logger;
     }
 
     public Result run()
     {
-
+	Result result = new Result();
     }
 
 
