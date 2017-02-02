0636eea3867f3c509f22de47fe3466f672468570
oONinaOo <loveninagold@gmail.com>
2017-02-02 09:14:30 +0100

    Update Result.java

--- Result.java
+++ Result.java
@@ -2,17 +2,26 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Collections;
+
 
 public class Result {
 
-	public List<Integer> topNumbers = new ArrayList<>();
+	public static List<String> topNumbers = new ArrayList<>();
 
-	int number1;
-	int number2;
-	int number3;
-	int number4;
-	int number5;
-	int number6;
+	public static int number1;
+	public static int number2;
+	public static int number3;
+	public static int number4;
+	public static int number5;
+	public static int number6;
 
 	public Result() {
 	
@@ -24,28 +33,41 @@ public class Result {
 		this.number6 = number6;
 	}
 
-	public static void getTopNumbers() {
+	public static List<String> getTopNumbers () {
+        	Path pathToFile = Paths.get("Simulation5.csv");
 
-	    String csvFile = "Simulation5.csv";
-            String line = "";
-            String cvsSplitBy = ",";
-		
-            try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+        	// create an instance of BufferedReader
+        	// using try with resource, Java 7 feature to close resources
+        	try (BufferedReader br = Files.newBufferedReader(pathToFile,
+                	StandardCharsets.US_ASCII)) {
 
-            	while ((line = br.readLine()) != null) {
+            	// read the first line from the text file
+            	String line = br.readLine();
 
-                    // use comma as separator
-                    String[] number = line.split(cvsSplitBy);
+            	// loop until all lines are read
+            	while (line != null) {
 
-                    System.out.println("[first= " + number[0] + ", secound= " + number[1] + ", third= " + number[2] + ", fourth= " + number[3] + ", fifth= " + number[4]+"]");
+                	// use string.split to load a string array with the values from
+                	// each line of
+                	// the file, using a comma as the delimiter
+                		String [] attributes = line.split(",");
+	
+                	// adding all numbers into ArrayList
+				for(int i=0; i<5; i++) {
+                			topNumbers.add(attributes[i]);
+				}
 
-            	}
+               		// read next line before looping
+                	// if end of file reached, line would be null
+                		line = br.readLine();
+            		}
 
-            } catch (IOException e) {
-            e.printStackTrace();
-            }	
-
-	}
-
-
+        	} catch (IOException ioe) {
+            		ioe.printStackTrace();
+        	}
+		Collections.sort(topNumbers);
+        return topNumbers;
+    }
+	
 }
+
